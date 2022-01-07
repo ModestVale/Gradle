307cabac328feb7eb9f5f6eeb1292ab95997bd38
Валентина Юрова <visible7@mail.ru>
2022-01-07 17:14:21 +0500

    Add unit tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateBonus()
+    {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 100;
+        int actual = service.remain(900);
+
+        assertEquals(actual, expected);
+
+        expected = 200;
+        actual = service.remain(800);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateBonusOnRangeBorder()
+    {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        assertEquals(actual, expected);
+
+        expected = 0;
+        actual = service.remain(1000);
+
+        assertEquals(actual, expected);
+    }
+}
